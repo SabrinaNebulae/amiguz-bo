b25173b9ad142a8a92b3ebe6e7c35e114affc6db
Sabrina B. <sabrina.b@nebulae-design.com>
2025-08-14 15:31:58 +0200

    test(Vite + Symfony + React)

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,17 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { registerControllers } from "vite-plugin-symfony/stimulus/helpers"
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { registerReactControllerComponents } from "vite-plugin-symfony/stimulus/helpers/react"
+
+registerReactControllerComponents(import.meta.glob('./react/controllers/**/*.js(x)\?')); 
 
 const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+registerControllers(
+  app,
+  import.meta.glob(
+    "./controllers/*_controller.js",
+    {
+      query: "?stimulus",
+      eager: true,
+    },
+  ),
+);
---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+import.meta.glob(["../img/**"]);
+
+import "../css/app.css";
+
+import './bootstrap.js';
